a7c9f3fd58465d78f9d28c1a058f69eda26e55e4
Dhia M <merzougui.dhia@gmail.com>
2022-02-21 12:06:44 +0100

    Minor CSS changes to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/Layout.js"
 import "../style/global.css"
-import {aboutCard} from "../style/about.module.css"
+import {aboutCard, aboutMe, education} from "../style/about.module.css"
 
 const AboutPage = () => {
   return (
@@ -9,15 +9,17 @@ const AboutPage = () => {
       <title>Light | About Me</title>
       <div className="gridContainer">
 
-      <section className = {aboutCard} id="aboutMe">
+      <section className = {aboutCard} id = {aboutMe}>
           <h1 className="greenText centeredText">About Me</h1>
+          <article className="aboutText">
           <p>I recently graduated with a bachelor's degree in Computer Science from the University of Caen Normandie. Throughout my years there, I've worked on many projects that have given me much experience on what it's like to work with a team and develop software on a deadline.</p>
           <p>On a personal level, I'm a geek at heart. If you can think of anything that's nerdy, whether that's anime, manga, tabletop games like Dungeons and Dragons, then chances are I'm already a fan, haha.</p>
           <p>I am very passionate about computer science, and have been since I was little. I'm ready to learn more from any opportunities given to me</p>
+          </article>
         </section>
 
         <section>
-          <article className = {aboutCard} id="education">
+          <article className = {aboutCard} id = {education}>
             <h1 className="greenText centeredText">Education</h1>
               <ul>
                 <li>
@@ -38,9 +40,9 @@ const AboutPage = () => {
             <ul>
               <li>Programming Languages: C, Java, Python, PHP, HTML, CSS, Javascript.</li>
               <li>Frameworks: Tensorflow, Django, Node JS, React JS, Vue JS, Gatsby, Electron.</li>
+              <li>Tools: Git/Github, Docker, Trello, Jira, Adobe XD</li>
               <li>Good understanding of Algorithms and Data Structures.</li>
               <li>Good understanding of Agile methodologies.</li>
-              <li>Tools: Git/Github, Docker, Trello, Jira, Adobe XD</li>
             </ul>
           </article>
         </section>
