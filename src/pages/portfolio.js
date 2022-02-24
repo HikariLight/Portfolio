de3405976813354e864d50ff97c44e125448e7da
Dhia M <merzougui.dhia@gmail.com>
2022-02-24 21:02:14 +0100

    Added github repo link for project

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -8,9 +8,9 @@ const PortfolioPage = () => {
     <Layout>
       <title>Light | Portfolio</title>
       <div className="gridContainer">
+        <ProjectCard name="Briefer" description="Chrome Extension that assists people with research by simplifying, summarising, and fusing web pages." tech="HTML, CSS and JS" link="https://github.com/HikariLight/Briefer" tryLink="https://briefer.netlify.app"></ProjectCard>  
         <ProjectCard name="KeyboardAuth" description="Authenticating users using Keyboard Dynamics" tech="React JS" link="https://github.com/HikariLight/KeyboardAuth" tryLink="https://keyboardauth.netlify.app"></ProjectCard>
         <ProjectCard name="HTR" description="Handwritten text recognition" tech="Python" link="https://github.com/HikariLight/HTR"></ProjectCard>
-        <ProjectCard name="Briefer" description="Chrome Extension that assists people with research by simplifying, summarising, and fusing web pages." tech="HTML, CSS and JS" tryLink="https://briefer.netlify.app"></ProjectCard>  
         <ProjectCard name="Story" description="A website for sharing short storiers and jokes" tech="PHP" link="https://github.com/HikariLight/Story" tryLink="https://story-hikarilight.herokuapp.com/"></ProjectCard>
       </div>
     </Layout>
