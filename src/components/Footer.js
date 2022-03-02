dbdda851a7cb60033f4038ac1c0ec59e86914eac
Dhia M <merzougui.dhia@gmail.com>
2022-03-02 18:39:09 +0100

    Updated Linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
         <p><span className="greenText">E-mail: </span><a href="mailto: merzougui.dhia@gmail.com">merzougui.dhia@gmail.com</a></p>
         <a href="https://github.com/HikariLight"><img className="icon" src="github.svg" alt="github icon"></img></a>
-        <a href="https://www.linkedin.com/in/dhia-eddine-merzougui-a5ba621b7/"><img className="icon" src="linkedin.svg" alt="LinkedIn icon"></img></a>
+        <a href="https://www.linkedin.com/in/dhia-merzougui/"><img className="icon" src="linkedin.svg" alt="LinkedIn icon"></img></a>
     </footer>
   )
 }
