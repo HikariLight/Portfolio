e197c2665d9d38f2b029898681dfb9401411554f
Dhia M <merzougui.dhia@gmail.com>
2022-03-23 14:11:34 +0100

    Updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,24 +6,5 @@ module.exports = {
   plugins: [
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-google-fonts",
-      options: {
-        fonts: ["Nunito"],
-        display: "swap"
-      }
-    },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "DhiaDev",
-        short_name: "DhiaDev",
-        start_url: "/",
-        background_color: "#F2F1E7",
-        theme_color: "#14971F",
-        display: "standalone",
-        icon: "static/favicon.png"
-      }
-    }
   ],
 };
