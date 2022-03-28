43339b9bb5bc88daaad054a51c6cbb54e88a95de
Dhia M <merzougui.dhia@gmail.com>
2022-03-28 21:49:14 +0200

    Added Helmet component and meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Helmet from "react-helmet"
 import Navbar from "./Navbar.js"
 import Footer from "./Footer.js"
 import "@fontsource/nunito"
@@ -6,6 +7,15 @@ import "@fontsource/nunito"
 const Layout = ({children}) => {
   return(
         <div>
+
+          <Helmet>
+            <meta charSet="utf-8"/>
+            <meta name="author" content="Dhia Merzougui"/>
+            <meta name="keywords" content="software developer, software engineer, web developer, programmer"/>
+            <meta name="description" content="Software engineer's portfolio website"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+          </Helmet>
+
             <Navbar />
 
             <main>
