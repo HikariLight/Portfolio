876eff659322b5499e4891dd1b2bdf6734e5800e
Dhia M <merzougui.dhia@gmail.com>
2022-03-28 21:50:36 +0200

    Minor props refactoring

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,27 +1,27 @@
 import * as React from "react"
 import {card, container, content, imgContainer, screenshot, links} from "../style/ProjectCard.module.css"
 
-const ProjectCard = ({name, description, tech, link, tryLink, imgLink}) => {
+const ProjectCard = ( props ) => {
   return (
         <section className = {card}>
 
           <div className = {container}>
             <article id={imgContainer}>
-              <img id = {screenshot} src = {imgLink} alt="screenshot"></img>
+              <img id = {screenshot} src = {props.imgLink} alt="screenshot"></img>
             </article>
 
             <article>
-              <h1 className="greenText">{name}</h1>
+              <h1 className="greenText">{props.name}</h1>
               <ul className = {content}>
-                  <li><span className = "greenText underlinedText">Description:</span> {description}</li>
-                  <li><span className = "greenText underlinedText">Technologies Used:</span> {tech}</li>
+                  <li><span className = "greenText underlinedText">Description:</span> {props.description}</li>
+                  <li><span className = "greenText underlinedText">Technologies Used:</span> {props.tech}</li>
               </ul>
             </article>
           </div>
 
           <div id = {links}>
-            { link && <a className="greenText" href = {link}>Code</a>}
-            { tryLink && <a className="greenText" href = {tryLink}>Try it!</a>}
+            { props.link && <a className="greenText" href = {props.link}>Code</a>}
+            { props.tryLink && <a className="greenText" href = {props.tryLink}>Try it!</a>}
           </div>
             
         </section>
