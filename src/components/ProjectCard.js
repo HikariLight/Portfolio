94d53b5167d93600a3026ab82e5e5657e4d25a99
Dhia M <merzougui.dhia@gmail.com>
2022-05-17 18:59:55 +0200

    portfolio links position change

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,30 +1,30 @@
 import * as React from "react"
-import {card, container, content, imgContainer, screenshot, links} from "../style/ProjectCard.module.css"
+import { card, container, content, imgContainer, screenshot, links } from "../style/ProjectCard.module.css"
 
-const ProjectCard = ( props ) => {
+const ProjectCard = (props) => {
   return (
-        <section className = {card}>
+    <section className={card}>
 
-          <div className = {container}>
-            <article id={imgContainer}>
-              <img id = {screenshot} src = {props.imgLink} alt="screenshot"></img>
-            </article>
+      <div className={container}>
+        <article id={imgContainer}>
+          <img id={screenshot} src={props.imgLink} alt="screenshot"></img>
+        </article>
 
-            <article>
-              <h1 className="greenText">{props.name}</h1>
-              <ul className = {content}>
-                  <li><span className = "greenText underlinedText">Description:</span> {props.description}</li>
-                  <li><span className = "greenText underlinedText">Technologies Used:</span> {props.tech}</li>
-              </ul>
-            </article>
+        <article>
+          <h1 className="greenText">{props.name}</h1>
+          <ul className={content}>
+            <li><span className="greenText underlinedText">Description:</span> {props.description}</li>
+            <li><span className="greenText underlinedText">Technologies Used:</span> {props.tech}</li>
+          </ul>
+
+          <div id={links}>
+            {props.link && <a className="greenText" href={props.link}>Code</a>}
+            {props.tryLink && <a className="greenText" href={props.tryLink}>Try it!</a>}
           </div>
+        </article>
+      </div>
 
-          <div id = {links}>
-            { props.link && <a className="greenText" href = {props.link}>Code</a>}
-            { props.tryLink && <a className="greenText" href = {props.tryLink}>Try it!</a>}
-          </div>
-            
-        </section>
+    </section>
   )
 }
 
